61ed83c91e0da853f241cc6c331088efb5876492
Aakash Adesara <aakash.adesara@gmail.com>
2015-01-25 04:23:31 -0800

    Revert c901d62..6a84bf4
    This rolls back to commit c901d6241b824629968fc7756060eea42847eaa2.

--- js/script.js
+++ js/script.js
@@ -5,20 +5,40 @@ $( document ).ready(function() {
 	
 	$("#upload").click(function(){
 
-		var countryName = $("#country").val();
-		var col = $("#col").val();
+		var validCountries = ["america", "mexico", "china", "india", "japan", "britain", "france", "russia", "greece", "australlia" ];
+		var validCols = ["food", "manners", "recreation", "beliefs", "appearance"];
+
+		var countryName = $("#country").val().toLowerCase();
+		var col = $("#col").val().toLowerCase();
 		var link = $("#link").val();
 		var description = $("#description").val();
 
-		var Extension = Parse.Object.extend(countryName);
-		country = new Extension();
+		var isValidCountry = false;
+		var isValidCols = true;
 
-		country.save({foo: col}).then(function(object) {
-		});
-		country.save({link: link}).then(function(object) {
-		});
-		country.save({description: description}).then(function(object) {
-		});
+		for(var i = 0; i < validCountries.length; i++){
+			if(validCountries[i] == countryName)
+				isValidCountry = true;
+		}
+		for(var i = 0; i < validCols.length; i++){
+			if(validCols[i] == col){
+				isValidCols = true;
+			}
+		}
+
+		if(isValidCols && isValidCountry){
+			var Extension = Parse.Object.extend(countryName.toLowerCase());
+			country = new Extension();
+
+			country.save({foo: col}).then(function(object) {
+			});
+			country.save({link: link}).then(function(object) {
+			});
+			country.save({description: description}).then(function(object) {
+			});
+		} else {
+			alert("Nope. Check country and col");
+		}
 	});
 
 });
