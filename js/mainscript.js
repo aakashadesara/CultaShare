5fbc29eadd189e591c66f3aa2ea187727192c05c
Aakash Adesara <aakash.adesara@gmail.com>
2015-01-25 00:42:30 -0800

    Button specification

--- js/mainscript.js
+++ js/mainscript.js
@@ -14,13 +14,14 @@ $( document ).ready(function() {
     $("#greece").click(function(){ country = "Greece"; rewind(country); });
     $("#australlia").click(function(){ country = "Australlia"; rewind(country); });
 
-
     rewind(country);
 });
 
 function rewind(country){
 	var div = document.getElementById('imageHolder');
 	div.innerHTML = "";
+
+	var buttonList = new Array();
 
 	document.getElementById('countriesNameTitle').innerHTML = country;
 
@@ -35,16 +36,96 @@ function rewind(country){
 	            	  "Desc: " + results[i].get("description")); */
 	            var link = results[i].get("link");
 	            var description = results[i].get("description");
+	            
+	            var col = results[i].get("foo");
+	            var addCol = true;
+	            
+	            for(var j = 0; j < buttonList.length; j++){
+	            	if(buttonList[j] == col){
+	            		addCol = false;
+	            	}
+	            }
+	            if(addCol){
+	            	buttonList.push(col);
+	            }
+	            addCol = true;
 	            //alert(link);
+
+
 
 	            var div = document.getElementById('imageHolder');
 				div.innerHTML += "<div class=\"pure-u-1-4\" ><img class=\"pure-img-responsive\" src=\"" + link + " alt=\"Image\"><div id=\"descDiv\" > <p style=\"text-align: center;\">" + description + "</p></div></div>";
 
 	        }
-	        
+	        document.getElementById("buttonHolders").innerHTML = " <button class=\"button-success pure-button\" class=\"narrowDown\" id=\"all\" style=\"margin: 5px\" >All</button>";
+	        for(var i = 0; i < buttonList.length; i++){
+		 		document.getElementById("buttonHolders").innerHTML += "<button class=\"button-success pure-button\" class=\"narrowDown\" id=\""+buttonList[i]+"\" style=\"margin: 5px\" >" + buttonList[i] + "</button>";
+		 	}
+
+		 	$("#food").click(function(){ rewindButton(country, "food"); });
+		 	$("#manners").click(function(){ rewindButton(country, "manners"); });
+		 	$("#recreation").click(function(){ rewindButton(country, "recreation"); });
+
 	    },
 	    error: function (error) {
 	        alert("Error: " + error.code + " " + error.message);
 	    }
 	});
+
+	
+}
+
+function rewindButton(country, specification){
+	var div = document.getElementById('imageHolder');
+	div.innerHTML = "";
+
+	var buttonList = new Array();
+
+	var Client = Parse.Object.extend(country);
+	var queryObject = new Parse.Query(Client);
+
+	queryObject.find({
+	    success: function (results) {
+	        for (var i = 0; i < results.length; i++) {
+	            
+	            var link = results[i].get("link");
+	            var description = results[i].get("description");
+
+	            var col = results[i].get("foo");
+	            var addCol = true;
+	            
+	            for(var j = 0; j < buttonList.length; j++){
+	            	if(buttonList[j] == col){
+	            		addCol = false;
+	            	}
+	            }
+	            if(addCol){
+	            	buttonList.push(col);
+	            }
+	            addCol = true;
+	            //alert(link);
+
+
+	            if(col == specification){
+	           	 	var div = document.getElementById('imageHolder');
+					div.innerHTML += "<div class=\"pure-u-1-4\" ><img class=\"pure-img-responsive\" src=\"" + link + " alt=\"Image\"><div id=\"descDiv\" > <p style=\"text-align: center;\">" + description + "</p></div></div>";
+				}
+	        }
+	        document.getElementById("buttonHolders").innerHTML = " <button class=\"button-success pure-button\" class=\"narrowDown\" id=\"all\" style=\"margin: 5px\" >All</button>";
+	        for(var i = 0; i < buttonList.length; i++){
+		 		document.getElementById("buttonHolders").innerHTML += "<button class=\"button-success pure-button\" class=\"narrowDown\" id=\""+buttonList[i]+"\" style=\"margin: 5px\" >" + buttonList[i] + "</button>";
+		 	}
+
+		 	$("#all").click(function(){ rewind(country); });
+		 	$("#food").click(function(){ rewindButton(country, "food"); });
+		 	$("#manners").click(function(){ rewindButton(country, "manners"); });
+		 	$("#recreation").click(function(){ rewindButton(country, "recreation"); });
+
+	    },
+	    error: function (error) {
+	        alert("Error: " + error.code + " " + error.message);
+	    }
+	});
+
+	
 }
